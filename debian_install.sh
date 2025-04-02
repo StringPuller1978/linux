951406b1dce58cda055a918f8caf7861d28ba96a
StringPuller1978 <130022327+StringPuller1978@users.noreply.github.com>
2025-04-02 13:45:50 +0800

    Update debian_install.sh

--- debian_install.sh
+++ debian_install.sh
@@ -20,3 +20,11 @@ curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /usr/
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt update
 apt install docker-ce docker-ce-cli containerd.io -y
+
+
+systemctl start docker
+systemctl enable docker
+
+docker volume create portainer_data
+
+docker run -d -p 8000:8000 -p 9443:9443 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
