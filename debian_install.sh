e751cb5bd2c40a3e7a2853ce6d052e83428f5c70
StringPuller1978 <130022327+StringPuller1978@users.noreply.github.com>
2025-04-02 13:41:10 +0800

    Update debian_install.sh

--- debian_install.sh
+++ debian_install.sh
@@ -1,3 +1,5 @@
+sudo usermod -aG sudo benson
+
 apt-get update
 sudo apt-get install curl wget htop iftop dnstop net-tools gvfs-backends python3 python3-pip unzip font-manager fcitx fcitx-tools fcitx-ui* fcitx-config-* fcitx-module* fcitx-frontend-* fcitx-m17n fcitx-table-array30 fcitx-chewing putty remmina vlc vlc-data gedit gparted
 
@@ -7,9 +9,14 @@ echo "XMODIFIERS=@im=fcitx" | sudo tee -a /etc/environment
 
 cp /usr/share/applications/fcitx.desktop .config/autostart/
 
-sudo usermod -aG sudo benson
 
 sudo sed -i 's/^#\?PermitRootLogin.*/PermitRootLogin yes/' /etc/ssh/sshd_config
 sudo systemctl restart sshd  # 或 `service sshd restart`
 grep "^PermitRootLogin" /etc/ssh/sshd_config
 sudo systemctl status sshd
+
+apt install apt-transport-https ca-certificates curl software-properties-common -y
+curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt update
+apt install docker-ce docker-ce-cli containerd.io -y
